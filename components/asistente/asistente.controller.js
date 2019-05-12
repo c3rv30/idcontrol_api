0dd7824e2df6051c8cca34351efb2d80c12e72de
Bryan Bobadilla <bbobadillapisero@gmail.com>
2019-05-12 14:53:50 -0400

    Contador Asistentes
    Solucion provisoria para contabilizar los asistentes del pasado y ultimo año, mes a mes.

--- components/asistente/asistente.controller.js
+++ components/asistente/asistente.controller.js
@@ -3,10 +3,15 @@ const Asistente = require('./asistente.model');
 
 module.exports = {
 
-  /* SOLUCION PROVISORIA
-    
+  /* ********** SOLUCION PROVISORIA **********
+    Contabiliza todos los asistentes de cada mes del presente y actual año
+    para su posterior comparacion en un grafico de barras en angular.
+
+    Solucion provisoria adaptada al actual modelo de datos,
+    se debe generar el nuevo modelo y adapatarlo a esa nueva estructura.
   */
-  asisCounter: async (req, res, next) => {
+
+  asisCounter: async (req, res) => {
     const { equipo } = req.body;
     // const startOfMonth = moment().startOf('month').format('YYYY/MM/DD');
     // const endOfMonth = moment().endOf('month').format('YYYY/MM/DD');
@@ -24,77 +29,25 @@ module.exports = {
           { $sort: { '_id.year': 1, '_id.month': 1 } },
         ],
       );
-      console.log(countNio);
 
+      const lastYearArray = [];
+      const currentYearArray = [];
       let i = 0;
-      let acumMesActual = 0;
-      const iMax = countAsis.length;
+      const iMax = countNio.length;
+
       for (; i < iMax; i += 1) {
-        const dateFormat = moment(countAsis[i].fecha, 'DD/MM/YYYY').format('YYYY/MM/DD');
-        const after = moment(new Date(dateFormat)).isAfter(startOfMonth); // true
-        if (after) {
-          acumMesActual += 1;
-          // console.log('La puta madre ivan');
+        const mon = moment().month(countNio[i]._id.month - 1).format('MMMM');
+
+        if (countNio[i]._id.year === parseInt(lastYear, 10)) {
+          lastYearArray.push({ month: mon, count: countNio[i].count });
+        } else {
+          currentYearArray.push({ month: mon, count: countNio[i].count });
         }
-        console.log('Date Format: ', moment(countAsis[i].fecha, 'DD/MM/YYYY').month());
       }
-
-
-
-      return res.status(200).json({ message: countNio });
-
-      // return next();
+      return res.status(200).send({ lastYearArray, currentYearArray });
     } catch (error) {
       console.log(error);
       return res.status(404).send({ message: 'error' });
     }
   },
-
 };
-
-
-
-
-/* sagdgldfhñjsdhdf´dflmñdfvl´mñdvsf´dvms´lvsl´mdvfd */
-
-/* if (countAsis) {
-        console.log('Array Length: ', countAsis.length);
-        // console.log('Asistentes: ', countAsis);
-        // Acumula asistentes del mes actual
-        let i = 0;
-        let acumMesActual = 0;
-        const iMax = countAsis.length;
-        for (; i < iMax; i += 1) {
-          const dateFormat = moment(countAsis[i].fecha, 'DD/MM/YYYY').format('YYYY/MM/DD');
-          const after = moment(new Date(dateFormat)).isAfter(startOfMonth); // true
-          if (after) {
-            acumMesActual += 1;
-            // console.log('La puta madre ivan');
-          }
-          console.log('Date Format: ', moment(countAsis[i].fecha, 'DD/MM/YYYY').month());
-        }
-        console.log('Asistentes del mes actual: ', acumMesActual);
-        return res.status(200).json({ message: 'ok' });
-      }
-      */
-// 'Colchagua CD'
-
-/* const countAsis = await Asistente.find(
-      {
-        $match:
-        {
-          fecha: { $gte: startOfMonth },
-          equipo,
-        },
-      },
-      {
-        $group: {
-          _id: '$equipo',
-          equipoCount: { $sum: 1 },
-        },
-      },
-    ); */
-
-// console.log(startOfMonth);
-// console.log(typeof now);
-// find({ airedAt: { $gte: '1987-10-19', $lte: '1987-10-26' } }).
